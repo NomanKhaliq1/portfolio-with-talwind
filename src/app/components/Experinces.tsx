da6128b23ec70c4182e32af3fe7a3d7b9f774f15
Noman Ghouri <35302984+NomanKhaliq1@users.noreply.github.com>
2024-12-14 03:48:37 +0500

    Update Experinces.tsx

--- src/app/components/Experinces.tsx
+++ src/app/components/Experinces.tsx
@@ -41,7 +41,7 @@ const experiences: Experience[] = [
 
 const ExperienceSection: FC = () => {
   return (
-    <div className="px-6 py-16 bg-gray-50">
+    <div className="px-6 py-20 bg-gray-50">
       <div className="text-center mb-6">
         <span className="inline-block px-4 py-2 rounded-full bg-gray-100 text-gray-700 text-sm font-medium">
           Experience
